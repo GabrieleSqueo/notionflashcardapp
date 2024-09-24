ecbc712ce69899f02bfe871568859de8c79033e0
unknown <gsqueo3@gmail.com>
2024-09-24 09:15:13 +0200

    Update route.js

--- app/api/initiate-notion-oauth/route.js
+++ app/api/initiate-notion-oauth/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const authorizationUrl = `https://api.notion.com/v1/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&response_type=code&owner=user&redirect_uri=${encodeURIComponent(process.env.OAUTH_REDIRECT_URI)}`;
+  const authorizationUrl = `https://api.notion.com/v1/oauth/authorize?client_id=10ad872b-594c-80e3-8fc6-0037f00ff279&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fnotionflashcard.com%2Flatest%2Fconnected`;
 
   // Return the authorization URL
   return NextResponse.json({ authorizationUrl });
